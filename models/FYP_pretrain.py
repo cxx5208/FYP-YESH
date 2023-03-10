39262435ebd65c84889fbd58d517177d70a67588
cxx5208 <76988460+cxx5208@users.noreply.github.com>
2023-03-10 12:23:46 +0530

    Update FYP_pretrain.py

--- models/FYP_pretrain.py
+++ models/FYP_pretrain.py
@@ -240,12 +240,8 @@ def FYP_pretrain(**kwargs):
     return model 
 
 
-@torch.no_grad()
 def concat_all_gather(tensor):
-    """
-    Performs all_gather operation on the provided tensors.
-    *** Warning ***: torch.distributed.all_gather has no gradient.
-    """
+
     tensors_gather = [torch.ones_like(tensor)
         for _ in range(torch.distributed.get_world_size())]
     torch.distributed.all_gather(tensors_gather, tensor, async_op=False)
